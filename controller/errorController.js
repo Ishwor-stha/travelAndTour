fb74793656ce7549e178abe104d42ba2925086fe
Ishwor-stha <ishwor.stha.bank@gmail.com>
2024-11-15 15:58:13 +0545

    error handling

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,4 +1,5 @@
 module.exports=(err,req,res,next)=>{
+    // all the error details comes from the user through ../utils/error handling
     res.status(err.statusCode).json({
         status:err.status,
         message:err.message,
