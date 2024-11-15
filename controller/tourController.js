5d12c9a7056d8261e1037e4fa1b24a34498d2c9a
Ishwor-stha <ishwor.stha.bank@gmail.com>
2024-11-15 12:28:16 +0545

    applying additional security features

--- controller/tourController.js
+++ controller/tourController.js
@@ -1,8 +1,8 @@
 const Tour = require("../modles/tourModel")
-module.exports.homePage=(req,res)=>{
+module.exports.homePage = (req, res) => {
     res.status(200).json({
-        status:"Success",
-        message:"This is main page"
+        status: "Success",
+        message: "This is main page"
     })
 }
 
@@ -17,8 +17,8 @@ module.exports.getTours = async (req, res) => {
         // let query={}
         let condition = [];
 
-        // destructring query parameters
-        let { destination, category, type, duration } = req.query;
+        // destructuring query parameters
+        let { destination, category, type, duration, name } = req.query;
 
         // Handling the sorting logic
         if (req.query.price) {
@@ -26,16 +26,17 @@ module.exports.getTours = async (req, res) => {
         }
 
         // Building the condition for $or condition
-        if (destination) condition.push({ destination: destination });
-        if (category) condition.push({ category: category });
-        if (type) condition.push({ type: type });
+        // new RegExp("pattern",flags) object is used to make a data case insensitive "i" stands for insensitive  
+        if (destination) condition.push({ destination: new RegExp(destination, 'i') });//i.e{ destination: /mustang/i }
+        if (category) condition.push({ category: new RegExp(category, 'i') });
+        if (type) condition.push({ type: new RegExp(type, 'i') });
         if (duration) condition.push({ duration: duration });
-
-        
+        if (name) condition.push({ name: new RegExp(name, 'i') });
+        // console.log(condition)
 
         // Fetching the data from the database using $or condition for flexible matching
         let tourQuery = Tour.find();
-        
+
         if (condition.length > 0) {
             // Use $or if any of the conditions are provided
             tourQuery = tourQuery.where({ $or: condition });
@@ -48,16 +49,16 @@ module.exports.getTours = async (req, res) => {
 
         const tour = await tourQuery;
 
-        if (tour.length>0) {
+        if (tour.length > 0) {
             res.status(200).json({
                 status: "Success",
                 tourList: tour
             });
         } else {
             res.status(200).json({
-                status:"Success",
-                tourList:"No tour found"
-            })
+                status: "Success",
+                tourList: "No tour found"
+            });
         }
     } catch (error) {
         res.status(404).json({
@@ -67,23 +68,43 @@ module.exports.getTours = async (req, res) => {
     }
 };
 
+
 //@EndPoint:localhost:6000/post-tour
 //@method:POST
 //@desc:Adding the tours
-module.exports.postTour=async (req,res)=>{
+module.exports.postTour = async (req, res) => {
     try {
-      
-        const newTour=await Tour.create(req.body);
-        res.status(201).json({
-            status:"Success",
-            NewTourDetails:newTour
-            
-        })
-        } catch (error) {
-            res.status(500).json({
-                status:"Failed",
-                message:error.message
-            })
-            
+        let data = {}
+        let keys = ["name", "price", "description", "destination", "image", "category", "type", "duration", "discount"]
+        //appling best practice to insert data by filtering
+        for (let key in req.body) {
+            // checks whether the req.body has the appropriate key
+            if (keys.includes(key)) {
+                // checking if the data is number or string
+                if (typeof req.body.key === "number") {
+                    data[key] = req.body[key]
+
+                }
+                else {
+                    // if the data is not number then convert it to the string for security concern
+                    data[key] = req.body[key].toString()
+                }
+
+
+            }
         }
+        // double check if the data.name is string or not
+        const newTour = await Tour.create(data);
+        res.status(201).json({
+            status: "Success",
+            NewTourDetails: newTour
+
+        })
+    } catch (error) {
+        res.status(500).json({
+            status: "Failed",
+            message: error.message
+        })
+
+    }
 }
