414a659143b0b18473ff80443a47819cd7e2dd94
Ishwor-stha <ishwor.stha.bank@gmail.com>
2024-11-24 15:39:06 +0545

    some changes

--- server.js
+++ server.js
@@ -52,33 +52,6 @@ async function databaseConnect() {
 
 // Call the database connection function
 databaseConnect();
-/****************************************************************************************************************** */
-// const storage = multer.diskStorage({
-//     destination: (req, file, cb) => {
-//         cb(null, 'uploads/');
-//     },
-//     filename: (req, file, cb) => {
-//         cb(null, Date.now() + '-' + file.originalname);
-//     }
-// });
-
-// const upload = multer({
-//     storage: storage,
-//     fileFilter: (req, file, cb) => {
-//         const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/jpg'];
-//         if (!allowedMimeTypes.includes(file.mimetype)) {
-//             return cb(new Error('Invalid file type, only JPEG, PNG, and JPG are allowed'), false);
-//         }
-//         cb(null, true);
-//     },
-//     limits: {
-//         fileSize: 1 * 1024 * 1024 // Limit file size to 1MB
-//     }
-// });
-
-// Export upload as part of an object
-// module.exports.upload = upload;
-/****************************************************************************************************************** */
 
 // Mount the tour route
 app.use("/", tourRoute);
