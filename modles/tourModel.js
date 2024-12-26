55c946a67efb65f6268770cc9ec6b2bd7eebc91a
Ishwor-stha <ishwor.stha.bank@gmail.com>
2024-12-26 11:05:13 +0545

    adding fields

--- modles/tourModel.js
+++ modles/tourModel.js
@@ -1,84 +1,111 @@
-const mongoose=require("mongoose");
-const slugify=require("slugify");
-const tourSchema=new mongoose.Schema({
-    name:{
-        type:"String",
-        required:[true,"Name of prodct  is missing"],
-        unique:[true,"Tour name already exists"]
+const mongoose = require("mongoose");
+const slugify = require("slugify");
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: "String",
+        required: [true, "Name of prodct  is missing"],
+        unique: [true, "Tour name already exists"]
     },
-    price:{
-        type:Number,
-        require:[true,"A Product price is missing"]
+    adult_price: {
+        type: Number,
+        required: [true, "A adult price is missing"]
     },
-    description:{
-        type:"String",
-        required:[true,"A Product should have Description"]
+    youth_price: {
+        type: Number,
+        required: [true, "A youth price is missing"]
     },
-    destination:{
-        type:"String",
-        required:[true,"A Tour Must have Destination"]
+    description: {
+        type: "String",
+        required: [true, "A Product should have Description"]
     },
-    image:{
-        type:"String",
-        required:[true," Photo of Tour is missing"]
+    placeName: {
+        type: "String",
+        required: [true, "A Tour Must have place name"]
     },
-    category:{
-        type:String,
-        required:[true,"Category is missing"]
+    image: {
+        type: "String",
+        required: [true, " Photo of Tour is missing"]
+    },
+    category: {
+        type: String,
+        required: [true, "Category is missing"]//luxury,holiday,vacation
 
     },
-    tour_type:{
-        type:String,
-        required:[true,"Tour is missing tour type (Domestic,International)"],
-        enum:["Domestic","International"]
+    tour_type: {
+        type: String,
+        required: [true, "Tour is missing tour type (Domestic,International)"],
+        enum: ["Domestic", "International"]
     },
-    count:{
-        type:Number
+    active_month: {
+        type: [String],
+        validate: {
+            validator: function (months) {
+                const validMonths = [
+                    "january", "february", "march", "april", "may", "june",
+                    "july", "august", "september", "october", "november", "december"
+                ];
+                for (const month of months) {
+                    if (!validMonths.includes(month.toLowerCase())) {
+                        return false;
+                    }
+                }
+                return true;
+            },
+            message: "Active month must contain valid month names"
+        }
     },
-    type:{
-        type:String,
-        default:"adventure"
-    },
-    duration:{
+    count: {
         type: Number,
-        required:[true,"Duration is Missing"],
-        validate:{
-            validator:function(value){
-                return typeof value==="number"
-            },
-            message:"Duration must be a number"
+        default: 0
+    },
+    total_number_of_people: {
+        type: Number,
+        required: [true, "A tour must have a total number of people"],
+        min: [1, "Total number of people must be at least 1"],
+        validate: {
+            validator: Number.isInteger,
+            message: "Total number of people must be an integer"
         }
     },
-    Discount:{
-        type:Number,
-        default:0,
-        validate:{
-            validator:function(discount){
-                return typeof discount==="number"
+    duration: {
+        type: Number,
+        required: [true, "Duration is Missing"],
+        validate: {
+            validator: function (value) {
+                return typeof value === "number"
             },
-            message:"Discount Must Be In Number"
+            message: "Duration must be a number"
         }
     },
-    slug:{
-        type:String
+    Discount: {
+        type: Number,
+        default: 0,
+        validate: {
+            validator: function (discount) {
+                return discount >= 0 && discount <= 100;
+            },
+            message: "Discount must be between 0 and 100"
+        }
+    },
+    slug: {
+        type: String
     }
 
 });
-tourSchema.pre("save",function(next){
-    if(this.isModified("name")){
-        this.slug=slugify(this.name);
-        next();
+tourSchema.pre("save", function (next) {
+    if (this.isModified("name")) {
+        this.slug = slugify(this.name);
     }
     next();
 
 })
-tourSchema.pre("findOneAndUpdate",function(next){
-    const update=this.getUpdate();
-    if(update.name){
-        update.slug=slugify(update.name);
+tourSchema.pre("findOneAndUpdate", function (next) {
+    const update = this.getUpdate();
+    if (update.name) {
+        update.slug = slugify(update.name);
     }
     next();
 });
 
-const Tour=mongoose.model("tour",tourSchema);
-module.exports=Tour;
+const Tour = mongoose.model("tour", tourSchema);
+module.exports = Tour;
