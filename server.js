a0e01a19474004e24a2cadcae48aaed3d2e29fbd
Ishwor-stha <ishwor.stha.bank@gmail.com>
2025-01-19 15:48:23 +0545

    added field on tour schema and doing the changes to the controller accordingly

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ dotenv.config();
 // Define the port
 
 //Start the server
-const PORT = process.env.PORT || 3000;
- app.listen(PORT, () => {
-     console.log(`Server is running on port ${PORT}`);
-     console.log(`App is running in ${process.env.NODE_ENV} mode`);
- });
-// module.exports=(req,res)=>app(req,res);
+// const PORT = process.env.PORT || 3000;
+//  app.listen(PORT, () => {
+//      console.log(`Server is running on port ${PORT}`);
+//      console.log(`App is running in ${process.env.NODE_ENV} mode`);
+//  });
+module.exports=(req,res)=>app(req,res);
