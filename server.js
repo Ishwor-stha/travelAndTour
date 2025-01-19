66ed6a27617f378725bc16cd6461cca0c37c24fa
Ishwor-stha <ishwor.stha.bank@gmail.com>
2025-01-19 15:59:01 +0545

    updating package

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 // Define the port
 
-//Start the server
+// Start the server
 // const PORT = process.env.PORT || 3000;
 //  app.listen(PORT, () => {
 //      console.log(`Server is running on port ${PORT}`);
